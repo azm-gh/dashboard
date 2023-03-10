2bfcecd637dc0956348eb8bbf5abd6dbc2c76529
Jakub Jehlík <jakubjehlik@Jakubs-MacBook-Air.local>
2023-03-10 15:08:34 +0100

    inital dasboard layout

---
+++ components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className ='flex justify-between px-4 pt-5'>
+        <h2>Dashboars</h2>
+        <h2>Welcome back, Jakub</h2>
+    </div>
+  )
+}
+
+export default Header
